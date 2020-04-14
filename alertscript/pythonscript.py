38e9d58f724b61a36f4ea0fd12c69fa4cb98419d
regenwatch <router@router.lan>
2020-04-14 17:28:09 +0100

    efiels

--- alertscript/pythonscript.py
+++ alertscript/pythonscript.py
@@ -4,14 +4,20 @@ import PyPDF2
 import re
 import sys
 
-urlsuffix = 31092 
+
+urlsuffix = 62730 
 blankcount = 0
 
 while blankcount <= 110:
       #url = (f'http://moderngov.southwark.gov.uk/documents/s88407/')
       #url = (f'http://moderngov.southwark.gov.uk/documents/s8{urlsuffix}/')
-      url = (f'https://committees.westminster.gov.uk/documents/s{urlsuffix}/')
+      #url = (f'https://committees.westminster.gov.uk/documents/s{urlsuffix}/')
+      #url = (f'http://democracy.cityoflondon.gov.uk/documents/s131659/')
+      #url = (f'http://moderngov.redbridge.gov.uk/documents/s{urlsuffix}/')
       #url = (f'http://democracy.towerhamlets.gov.uk/documents/s166125/')
+      #url = (f'https://moderngov.sutton.gov.uk/documents/s63277/')
+      https://democracy.islington.gov.uk/documents/s21033/
+      https://modgov.hillingdon.gov.uk/documents/s47790/
       urlsuffix += 1
       r = requests.get(url)
 
@@ -25,25 +31,22 @@ while blankcount <= 110:
                 f.write(r.content)
         print("pdf found")
         # Open the pdf file
-        object = PyPDF2.PdfFileReader("/tmp/metadata.pdf")
+        object = PyPDF2.PdfFileReader("/tmp/metadata.pdf", strict=False)
 
         # get number of pages
         NumPages = object.getNumPages()
         print(NumPages)
         
-        if NumPages <= 50:
-            f = open('filename.md', 'w')
-            # extract text and do the search
-            for i in range(0, NumPages):
-                PageObj = object.getPage(i)
-                print("this is page " + str(i)) 
-                Text = PageObj.extractText() 
-                # print(Text)
-                EstateSearch = re.search(('estate'), Text, re.IGNORECASE)
-                SecondarySearch = re.search(('redevelopment|renewal|demolition|regeneration'), Text, re.IGNORECASE)
-                if EstateSearch and SecondarySearch:
-                    print("yes word combination found")
-                    f.write(url)
-        else: break
-             
-f.close()
+        f = open('filename.md', 'w')
+        # extract text and do the search
+        for i in range(0, NumPages):
+            PageObj = object.getPage(i)
+            print("this is page " + str(i)) 
+            Text = PageObj.extractText() 
+            # print(Text)
+            EstateSearch = re.search(('estate'), Text, re.IGNORECASE)
+            SecondarySearch = re.search(('redevelopment|renewal|demolition|regeneration'), Text, re.IGNORECASE)
+            if EstateSearch and SecondarySearch:
+                print("yes word combination found")
+                f.write(url)
+        f.close()
